78949a161e2942af5d4d9cc50deda97fbf89bedf
WaspZV <stantiet@mail.ru>
2025-02-26 10:40:15 +0700

    html,and renderTemplate

--- app/routes.py
+++ app/routes.py
@@ -1,5 +1,9 @@
 from app import app
+from flask import render_template
 
 @app.route('/')
+
+@app.route('/index')
 def index():
-    return "Hello world!"
+    user = {'username': 'Pavel'}
+    return render_template('index.html', title='Home', user=user)
