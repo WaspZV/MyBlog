32eaafd1f56ccbe44147eb974b155e2a1e03b0ea
WaspZV <stantiet@mail.ru>
2025-02-28 13:23:10 +0700

    ADD LOGIN HOME BUTTON

--- app/routes.py
+++ app/routes.py
@@ -1,6 +1,9 @@
 from app import app
 from flask import render_template
 
+from app.forms import LoginForm
+
+
 @app.route('/')
 
 @app.route('/index')
@@ -17,3 +20,8 @@ def index():
         }
     ]
     return render_template('index.html', title='Home', user=user, posts=posts)
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    form = LoginForm()
+    return render_template('login.html', title='Sign In', form=form)
